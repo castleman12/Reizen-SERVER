1eae30c100d1f91e721d7a37538bfc4298339048
castleman12 <68916886+castleman12@users.noreply.github.com>
2021-01-04 13:59:00 -0600

    heroku deployment

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ app.use('/trip', validateSession,  trip)
 app.use('/packingList', packingList)
 app.use('/packingItems', packingItems)
 
-app.listen(3000, function() {
-    console.log('App is listening on port 3000')
+app.listen(process.env.PORT, () => {
+    console.log('App is listening on port ${process.env.PORT')
 })
--- db.js
+++ db.js
@@ -2,7 +2,7 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    host: 'localhost',
+  
     dialect: 'postgres'
 })
 
