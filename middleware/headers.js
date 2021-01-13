5adaf0cbbe0f1a266810a5cf604dc41edf3920e4
castleman12 <68916886+castleman12@users.noreply.github.com>
2021-01-12 22:35:05 -0600

    changed headers

--- middleware/headers.js
+++ middleware/headers.js
@@ -1,9 +1,6 @@
 module.exports = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://jw-reizen.herokuapp.com');
-  response.setHeader("Access-Control-Allow-Credentials", "true");
-  res.header('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'DELETE');
-  
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 }
